26d9b772de3cf9e236eb6c0760c4ed4bd4b2257c
tusharr78 <88523236+tusharr78@users.noreply.github.com>
2021-10-17 13:47:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -118,7 +118,12 @@ app.route("/:params")
 );
 
   
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
         
-app.listen(3000, function(){
-    console.log("Server is up and running on port 3000");
+app.listen(port, function(){
+    console.log("Server started successfully");
 })
